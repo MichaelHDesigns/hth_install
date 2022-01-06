a9e0e8a17736f66272e71b0df1b862c33ccc6d33
MichaelHDesigns <43361618+MichaelHDesigns@users.noreply.github.com>
2022-01-06 00:32:46 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,16 +6,6 @@ su bitcore
 HOME=/bitcore
 NODE_VERSION=4.0.0
 NVM_VERSION=0.33.2
-NVM_DIR=$HOME/.nvm
-
-curl https://raw.githubusercontent.com/creationix/nvm/v$NVM_VERSION/install.sh | bash && source $NVM_DIR/nvm.sh && nvm install $NODE_VERSION && nvm alias default $NODE_VERSION && nvm use default
-
-cd $HOME && source $NVM_DIR/nvm.sh && nvm use v4.0.0 && mkdir -p $HOME/.bitcore/data && ln -s /usr/bin/helpthehomelessd $HOME/.bitcore/data && git clone https://github.com/dashpay/bitcore-dash -b nodownload \
-    && cd bitcore-dash && npm install dashpay/insight-api-dash#master_jaxx --save 
-    && npm install -g
-
-cd $HOME && echo "#!/bin/bash" >> launch_bitcore-dash.sh && echo "source $NVM_DIR/nvm.sh" >> launch_bitcore-dash.sh && echo "cd $NVM_DIR/versions/node/v4.0.0/bin/" >> launch_bitcore-dash.sh \
-    && echo "bitcored" >> launch_bitcore-dash.sh && chmod a+x launch_bitcore-dash.sh
-
-Then start with : 
-./bitcore/launch_bitcore-dash.sh
+NVM_DIR=$HOME/.nvm && curl https://raw.githubusercontent.com/creationix/nvm/v$NVM_VERSION/install.sh | bash && source $NVM_DIR/nvm.sh && nvm install $NODE_VERSION && nvm alias default $NODE_VERSION && nvm use default && cd $HOME && source $NVM_DIR/nvm.sh && nvm use v4.0.0 && mkdir -p $HOME/.bitcore/data && ln -s /usr/bin/helpthehomelessd $HOME/.bitcore/data && git clone https://github.com/dashpay/bitcore-dash -b nodownload && && cd bitcore-dash && npm install dashpay/insight-api-dash#master_jaxx --save 
+&& npm install -g && cd $HOME && echo "#!/bin/bash" >> launch_bitcore-dash.sh && echo "source $NVM_DIR/nvm.sh" >> launch_bitcore-dash.sh && echo "cd $NVM_DIR/versions/node/v4.0.0/bin/" >> launch_bitcore-dash.sh \
+&& echo "bitcored" >> launch_bitcore-dash.sh && chmod a+x launch_bitcore-dash.sh && Then start with : ./bitcore/launch_bitcore-dash.sh
